10e3734c7603d58cdd9f1575d08dd7606014804c
Henry Campos <henry.campos@galileo.edu>
2018-09-09 00:30:21 -0600

    Started with user-profile page

--- build/php/modules/nav.php
+++ build/php/modules/nav.php
@@ -10,18 +10,25 @@
                         <img src="../../assets/img/users/profile.png">
                     </span>
                     <li class="nav-user">
-                        <a href="">
+                        <a>
                             <span class="user-alias"><b>@</b>henry.campos97</span>
                         </a>
                         <span class="profile-link">Ver más</span>
                     </li>
-                    <li><a href="../shop/home.php" class="nav-option"><span class="option-icon"><i class="fas fa-home"></i></span></a></li>
+                    <li><a href="../groups/home.php" class="nav-option"><span class="option-icon"><i class="fas fa-home"></i></span></a></li>
                     <!-- En el span .counter se debe colocar el tamaño del detalle. -->
                     <li><a href="../shop/shopping_cart.php" class="nav-option"><span class="option-icon notification"><i class="fas fa-bell"></i></span><span class="counter">2</span></a></li>
                     <li><a href="" class="nav-option"><span class="option-icon"><i class="fas fa-sign-out-alt"></i></span></a></li>
                 </span>
             </ul>
         </section>
-     
   </body>
 </html>
+
+<script>
+$(document).ready(function(){
+    $('.nav-user').click(function(){
+        window.location.href = "../users/profile.php";
+    });
+});
+</script>
---
+++ build/php/users/profile.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Inicio</title>
+    <?php
+        include '../modules/links.php';
+    ?>
+</head>
+<?php
+    include '../modules/nav.php';
+?>
+<body>
+    <section class="profile-section">
+        <span class="central-container">
+            <span class="profile-title">
+                <span class="profile-img">
+                    <img src="../../assets/img/users/profile.png">
+                    <span class="img-update"><i class="fas fa-file-upload"></i> Subir imagen</span>
+                </span>
+                <span class="name-container">
+                    <span class="user-lastnames">
+                        Campos Ogáldez
+                    </span>
+                    <span class="user-names">
+                        Henry Alejandro
+                    </span>
+                    <span class="user-alias">
+                        <b>@</b>henry.campos97
+                    </span>
+                    <span class="button-section">
+                        <a class="btn-login">Seguir</a>
+                    </span>
+                </span>
+            </span>
+        </span>
+    </section>
+</body>
