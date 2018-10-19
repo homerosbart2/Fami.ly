507c8212d88579b7be62a8cb4c11486ec4a695c5
Diego alejandro Alay <diego.alay@galileo.edu>
2018-10-19 17:07:24 -0600

    notificaciones

--- build/php/modules/notification-wall.php
+++ build/php/modules/notification-wall.php
@@ -28,7 +28,7 @@
                 if(inserted == 0){
                     rows += `<span class="notification">`;
                     rows += `<span class="user-name">${user}</span>`;
-                    rows += `<a class="delete-notification"  id= "` + id + `"><i class="far fa-eye-slash"></i></a>`;
+                    rows += `<a class="delete-notification"><i class="far fa-eye-slash"></i></a>`;
                     rows += `<span class="type"><i class="fas fa-circle"></i> ${type}</span>`;
                     rows += `<span notification-id="${id}" class="text">${text}</span>`;
                     rows += `</span>`;
@@ -53,7 +53,7 @@
             rows += `</span>`;
             rows += `<span class="notification">`;
             rows += `<span class="user-name">${user}</span>`;
-            rows += `<a class="delete-notification" id= "` + id + `"><i class="far fa-eye-slash"></i></a>`;
+            rows += `<a class="delete-notification"><i class="far fa-eye-slash"></i></a>`;
             rows += `<span class="type"><i class="fas fa-circle"></i> ${type}</span>`;
             rows += `<span notification-id="${id}" class="text">${text}</span>`;
             rows += `</span>`;
@@ -72,30 +72,31 @@
     //Función que elimina la notificación.
     //TODO: Hay que eliminar la notificación de la base de datos, pero se necesita tomar en cuenta el id para esto, me comentas cuando vayas por esta parte.
     function removeNotification(lmnt){
-        notifyId = lmnt.attr("id");
-        //cambiamos el estado de la notificacion a leida
-        $.ajax({
-            url: "../rutas_ajax/notificaciones/cambiar_estado.php?notificacion=" + notifyId,
-            type: "POST",
-            success: function(r){
-                // alert(r);
-                if(r == 1){
-                    object = lmnt.parent().parent();
-                    if(object.find('.notification').length == 1){
-                        if(object.parent().find('.group-notification').length == 1){
-                            emptyNotificationWall();
-                        }
-                        object.remove();
-                    }else{
-                        lmnt.parent().remove();
-                        object = object.find('.number');
-                        object.html((parseInt(object.text().split(' ')[0]) - 1) + ' NUEVAS');
-                    }
-                    object = $('#nav-counter');
-                    object.html(parseInt(object.text()) - 1);
+        lmnt.parent().find('.text').each((index, element)=>{
+            notifyId = $(element).attr('notification-id');
+            //cambiamos el estado de la notificacion a leida
+            $.ajax({
+                url: "../rutas_ajax/notificaciones/cambiar_estado.php?notificacion=" + notifyId,
+                type: "POST",
+                success: function(r){
                 }
+            });  
+        });
+         
+
+        object = lmnt.parent().parent();
+        if(object.find('.notification').length == 1){
+            if(object.parent().find('.group-notification').length == 1){
+                emptyNotificationWall();
             }
-        });          
+            object.remove();
+        }else{
+            lmnt.parent().remove();
+            object = object.find('.number');
+            object.html((parseInt(object.text().split(' ')[0]) - 1) + ' NUEVAS');
+        }
+        object = $('#nav-counter');
+        object.html(parseInt(object.text()) - 1);   
     }
 
     //Eventos del mural de notificaciones y la generación de notificaciones depende de esta función.
